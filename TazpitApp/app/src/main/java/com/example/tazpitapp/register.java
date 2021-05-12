3a23f061cdf7490ff121d34eb1b90799d4ac9bb2
erdat <yosirata2016@gmail.com>
2021-05-12 22:47:27 +0300

    new regsier

--- TazpitApp/app/src/main/java/com/example/tazpitapp/register.java
+++ TazpitApp/app/src/main/java/com/example/tazpitapp/register.java
@@ -12,18 +12,78 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+///////////////////////////
 
+import com.google.android.material.textfield.TextInputLayout;
+///////////
 
 public class register extends AppCompatActivity {
-    private static final String[] CITYS = new String[]{
-            "Afghanistan", "Albania", "Algeria", "Andorra", "Angola"
-    };
-
+    TextInputLayout til_city;
+    AutoCompleteTextView act_city;
+    ArrayList <String> ArrList=new ArrayList<>();
+    ArrayList<String> arrayList_city;
+    ArrayAdapter<String> arrayAdapter_city;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
-        String[] citys = getResources().getStringArray(R.array.citysOnTest);
+        til_city=(TextInputLayout)findViewById((R.id.til_city));
+        act_city=(AutoCompleteTextView)findViewById((R.id.act_city));
+        get_json();
+        arrayAdapter_city=new ArrayAdapter<>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,ArrList);
+        act_city.setAdapter(arrayAdapter_city);
+        act_city.setThreshold(1);
+
+    }
+    public  void get_json(){
+        String json;
+        try{
+            InputStream is =getAssets().open("citys.json");
+            int size =is.available();
+            byte[] buffer=new byte [size];
+            is.read(buffer);
+            is.close();
+            json =new String(buffer,"UTF-8");
+            JSONArray jsonArray= new JSONArray(json);
+            for(int i =0 ;i< jsonArray.length();i++){
+                JSONObject obj=jsonArray.getJSONObject(i);
+                ArrList.add(obj.getString("cityName"));
+
+            }
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+//package com.example.tazpitapp;
+//
+//import android.os.Bundle;
+//import android.widget.ArrayAdapter;
+//import android.widget.AutoCompleteTextView;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.ArrayList;
+//
+//
+//public class register extends AppCompatActivity {
+//    private static final String[] CITYS = new String[]{
+//            "Afghanistan", "Albania", "Algeria", "Andorra", "Angola"
+//    };
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.register);
+//        String[] citys = getResources().getStringArray(R.array.citysOnTest);
 
 
 
@@ -62,10 +122,10 @@ catch (JSONException | IOException e){
 
 
 
-        AutoCompleteTextView editText = findViewById(R.id.actv);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                R.layout.custom_list_item, R.id.text_view_list_item, citys);
-        editText.setAdapter(adapter);
+//        AutoCompleteTextView editText = findViewById(R.id.actv);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                R.layout.custom_list_item, R.id.text_view_list_item, citys);
+//        editText.setAdapter(adapter);
 
 
 
@@ -73,5 +133,5 @@ catch (JSONException | IOException e){
 
         //get the input like for a normal EditText
         //String input = editText.getText().toString();
-    }
-}
+//    }
+//}
