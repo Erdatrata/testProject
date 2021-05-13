7cd6f660620f0be2893cd6917dac75d3a2f3ba44
erdat <yosirata2016@gmail.com>
2021-05-14 01:37:03 +0300

    Logic Login Activity go Settings Activity

--- TazpitApp/app/src/main/java/com/example/tazpitapp/LoginActivity.java
+++ TazpitApp/app/src/main/java/com/example/tazpitapp/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.example.tazpitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.time.Instant;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        login = (Button) findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity(new Intent(LoginActivity.this, SettingActivity.class));
+            }
+        });
+
     }
 }
