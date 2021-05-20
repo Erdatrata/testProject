ccfd82f264a112a1bd0c9ea4a489e0eda6116603
Gabi <gabigutkin@gmail.com>
2021-05-21 01:00:24 +0300

    adding title and desc to firestore. need to continue

--- TazpitApp/app/src/main/java/com/example/tazpitapp/fillReport.java
+++ TazpitApp/app/src/main/java/com/example/tazpitapp/fillReport.java
@@ -1,5 +1,6 @@
 package com.example.tazpitapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,6 +12,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -19,15 +21,25 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class fillReport extends AppCompatActivity {
@@ -38,7 +50,11 @@ public class fillReport extends AppCompatActivity {
     CheckBox credit;
     //ImageView checkUploadImage;
     Bitmap bm;
+    Uri download_url;
+    public static final String TITLE_KEY="title";
+    public static final String DESCRIPTION_KEY="description";
     private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private DocumentReference mDocRef= FirebaseFirestore.getInstance().document("users reports/"+UUID.randomUUID());
 
     private static final int SELECT_PHOTO = 1;
 
@@ -74,8 +90,26 @@ public class fillReport extends AppCompatActivity {
 //                        Toast.makeText(getApplicationContext(), "title: " + getTitle + "\n desc: " + getDescription + "\n chekbox chosen ", Toast.LENGTH_SHORT).show();
 //                    else
 //                        Toast.makeText(getApplicationContext(), "title: " + getTitle + "\n desc: " + getDescription + "\n chekbox not chosen ", Toast.LENGTH_SHORT).show();
+                        //uploading title and description to firestore firebase============
+                    Map<String,Object> dataToSave=new HashMap<String,Object>();
+                    dataToSave.put(TITLE_KEY,getTitle);
+                    dataToSave.put(DESCRIPTION_KEY,getDescription);
+                    //dataToSave.put("ImageUrl", "https://firebasestorage.googleapis.com/v0/b/tazpit-testingandprototype.appspot.com/o/firememes%2F%D7%A0%D7%99%D7%A1%D7%95%D7%99%20%D7%A0%D7%99%D7%A1%D7%95%D7%99%2F25d8f449-70ba-448d-a56d-d59128b2ab38.png?alt=media&token=827aa828-cb4c-49a5-b3b3-f5d1c2e2c1fa");
+                    mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Log.d("InspiritingQuote", "DocumentSnapshot successfully written!");
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull @NotNull Exception e) {
+                            Log.w("InspiritingQuote", "Error writing document", e);
+                        }
+                    });
 
-                    //19.5 try to upload image to firebase===========================
+                    //==============================================================
+
+                    //uploading the image to storage firebase===========================
                     ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
                     bm.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
                     byte [] data=outputStream.toByteArray();
@@ -83,12 +117,13 @@ public class fillReport extends AppCompatActivity {
                     StorageReference firememeRef = storage.getReference(path);
                     StorageMetadata metadata=new StorageMetadata.Builder().setCustomMetadata("caption", "the photo").build();
                     UploadTask uploadTask=firememeRef.putBytes(data, metadata);
+
                     //if(uploadTask.isSuccessful())
                     Toast.makeText(getApplicationContext(), "image upload succeed ", Toast.LENGTH_LONG).show();
 
 
 
-                    //19.5 try to upload image to firebase===========================
+                    //==========================================
 
                 }
 
