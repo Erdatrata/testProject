c1f4d0392f4c95773a581d70499022c71e1f5caf
Gabi <gabigutkin@gmail.com>
2021-05-22 01:05:16 +0300

    fillreport update

--- TazpitApp/app/src/main/java/com/example/tazpitapp/fillReport.java
+++ TazpitApp/app/src/main/java/com/example/tazpitapp/fillReport.java
@@ -26,6 +26,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
@@ -50,14 +51,14 @@ public class fillReport extends AppCompatActivity {
     Button submit;
     ImageButton pickMedia;
     CheckBox credit;
-    //ImageView checkUploadImage;
     Bitmap bm;
-    String returnUrl;
+    String returnUrl="";
     public static final String TITLE_KEY = "title";
     public static final String DESCRIPTION_KEY = "description";
     private FirebaseStorage storage = FirebaseStorage.getInstance();
-    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("users reports/" + UUID.randomUUID());
-
+    private DocumentReference mDocRef;
+    private FirebaseAuth mAuth;
+    Map<String, Object> dataToSave = new HashMap<String, Object>();
     private static final int SELECT_PHOTO = 1;
 
     @Override
@@ -69,8 +70,9 @@ public class fillReport extends AppCompatActivity {
         submit = (Button) findViewById(R.id.fill_report_send_button);
         pickMedia = (ImageButton) findViewById(R.id.fill_report_upload_button);
         credit = (CheckBox) findViewById(R.id.fill_report_add_credit);
-        // checkUploadImage=(ImageView)findViewById(R.id.check_upload_phto);
-        // checkUploadImage = new ImageView(this);
+        mAuth = FirebaseAuth.getInstance();
+        mDocRef = FirebaseFirestore.getInstance().document("users reports/" + mAuth.getCurrentUser().getEmail());
+
         pickMedia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,54 +90,29 @@ public class fillReport extends AppCompatActivity {
                 else {
                     String getTitle = title.getText().toString();
                     String getDescription = description.getText().toString();
-                    Map<String, Object> dataToSave = new HashMap<String, Object>();
-                    if (itemClicked(credit) == true)
-                        dataToSave.put("credit", true);
-                    else
-                        dataToSave.put("credit", false);
-                    dataToSave.put("media url", returnUrl);
-                    dataToSave.put(DESCRIPTION_KEY, getDescription);
-                    dataToSave.put(TITLE_KEY, getTitle);
-                    //dataToSave.put("ImageUrl", "https://firebasestorage.googleapis.com/v0/b/tazpit-testingandprototype.appspot.com/o/firememes%2F%D7%A0%D7%99%D7%A1%D7%95%D7%99%20%D7%A0%D7%99%D7%A1%D7%95%D7%99%2F25d8f449-70ba-448d-a56d-d59128b2ab38.png?alt=media&token=827aa828-cb4c-49a5-b3b3-f5d1c2e2c1fa");
-                    mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Log.d("InspiritingQuote", "DocumentSnapshot successfully written!");
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull @NotNull Exception e) {
-                            Log.w("InspiritingQuote", "Error writing document", e);
-                        }
-                    });
 
                     //==============================================================
 
-                    //uploading the image to storage firebase===========================
+                    //uploading the image to storage firebase===============================
                     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
                     bm.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
                     byte[] data = outputStream.toByteArray();
-                    String path = "firememes/" + "ניסוי ניסוי" + "/" + UUID.randomUUID() + ".png";
+                    String path = "firememes/" + mAuth.getCurrentUser().getEmail() + "/" + UUID.randomUUID() + ".png";
                     StorageReference firememeRef = storage.getReference(path);
                     StorageMetadata metadata = new StorageMetadata.Builder().setCustomMetadata("caption", "the photo").build();
                     UploadTask uploadTask = firememeRef.putBytes(data, metadata);
-                    // if(uploadTask.isSuccessful())
-                    Toast.makeText(getApplicationContext(), "image upload succeed ", Toast.LENGTH_LONG).show();
 
                     uploadTask.addOnCompleteListener(fillReport.this, new OnCompleteListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onComplete(@NonNull @NotNull Task<UploadTask.TaskSnapshot> task) {
-                            Log.i("MA", "upload task completed");
+                            if(task.isSuccessful()) {
+                                Log.i("MA", "upload image task to storage completed");
+                            }
                         }
                     });
 
-                    MediaDownloadUral(uploadTask, firememeRef);
-
-
-                    //==========================================
-
+                    uploadReportToFireStore(uploadTask, firememeRef, getDescription, getTitle);
                 }
-
             }
         });
 
@@ -145,21 +122,14 @@ public class fillReport extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == SELECT_PHOTO) {
-            Toast.makeText(getApplicationContext(), "after pick", Toast.LENGTH_LONG).show();
-            //below checks i can get the media and present in on ImageView
+            Log.d("after pick media", "succced pick media");
             Uri imgUri = data.getData();
             try {
                 bm = MediaStore.Images.Media.getBitmap(getContentResolver(), imgUri);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-//                        try {
-//                 bm = MediaStore.Images.Media.getBitmap(getContentResolver(), imgUri);
-//                checkUploadImage.setImageBitmap(bm);} catch (FileNotFoundException e) {
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
+
         }
     }
 
@@ -173,7 +143,7 @@ public class fillReport extends AppCompatActivity {
         return indc;
     }
 
-    public void MediaDownloadUral(UploadTask uploadTask,StorageReference firememeRef) {
+    public void uploadReportToFireStore(UploadTask uploadTask,StorageReference firememeRef,String getDescription, String getTitle) {
         Task<Uri> getDownloadUriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
        @Override
           public Task<Uri> then(@NonNull @NotNull Task<UploadTask.TaskSnapshot> task) throws Exception {
@@ -188,9 +158,32 @@ public class fillReport extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull @NotNull Task<Uri> task) {
                 if (task.isSuccessful()) {
+                    if (itemClicked(credit) == true)
+                        dataToSave.put("credit", true);
+                    else
+                        dataToSave.put("credit", false);
+                    dataToSave.put(DESCRIPTION_KEY, getDescription);
+                    dataToSave.put(TITLE_KEY, getTitle);
                     Uri downloadUri = task.getResult();
-                    returnUrl=downloadUri.toString();
+                    returnUrl+=downloadUri.toString();
                     Log.d("downloaduri", returnUrl);
+                    dataToSave.put("media url", returnUrl);
+                    mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Log.d("InspiritingQuote", "DocumentSnapshot successfully written!");
+                            Toast.makeText(getApplicationContext(), "העלאת האירוע בוצעה בהצלחה", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(fillReport.this, MainActivity.class);
+                            startActivity(intent);
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull @NotNull Exception e) {
+                            Log.w("InspiritingQuote", "Error writing document", e);
+                        }
+                    });
+
 
                 }
             }
