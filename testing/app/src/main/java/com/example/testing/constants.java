a554593d09008469482b85d3dbade4eb3f5a1eed
elie <ezezer2@hotmail.com>
2021-05-24 19:06:32 +0300

    notification when in range

--- testing/app/src/main/java/com/example/testing/constants.java
+++ testing/app/src/main/java/com/example/testing/constants.java
@@ -5,4 +5,7 @@ class constants {
     static  final String ACTION_START_LOCATION_SERVICE="startLocationService";
     static final String ACTION_STOP_LOCATION_SERVICE="stopLocationService";
 
+
+    static final String NOTIFICATION ="notfication";
+
 }
