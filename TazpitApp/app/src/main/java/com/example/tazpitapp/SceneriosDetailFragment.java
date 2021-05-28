a3399397b91f62262d124d05f458ff38f80512a7
Evja <e>
2021-05-28 11:51:02 +0300

    Made several changes! (details inside)
    -Started moving strings to R.strings file (currently only from settings and homepage menu)
    -Put array for days  and days' R.id in constants
    -taking settings from server upon login

--- TazpitApp/app/src/main/java/com/example/tazpitapp/SceneriosDetailFragment.java
+++ TazpitApp/app/src/main/java/com/example/tazpitapp/SceneriosDetailFragment.java
@@ -50,11 +50,11 @@ public class SceneriosDetailFragment extends Fragment {
             // to load content from a content provider.
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
-            Activity activity = this.getActivity();
-            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
-            if (appBarLayout != null) {
-                appBarLayout.setTitle(mItem.content);
-            }
+//            Activity activity = this.getActivity();
+//            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+//            if (appBarLayout != null) {
+//                appBarLayout.setTitle(mItem.content);
+//            }
         }
     }
 
